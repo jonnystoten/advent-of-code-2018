eb6178bb6b4e63ead901cee8443a0e927e6a0e83
Jonny Stoten <jonny@jonnystoten.com>
2019-01-02 23:35:30 +0000

    Day 15 working :O

--- 15/15.exs
+++ 15/15.exs
@@ -1,19 +1,34 @@
 defmodule Unit do
-  defstruct [:position, :species, hp: 200, attack: 3]
+  defstruct [:id, :position, :species, hp: 200, attack: 3]
 
   def draw(%Unit{species: :elf}), do: "E"
   def draw(%Unit{species: :goblin}), do: "G"
 end
 
 defmodule State do
-  defstruct [:bounds, walls: MapSet.new(), units: []]
+  defstruct [:bounds, walls: MapSet.new(), units: %{}, next_id: 0]
+
+  def winner(%State{units: units}) do
+    units
+    |> Map.values()
+    |> Enum.map(& &1.species)
+    |> hd()
+  end
+
+  def total_hp(%State{units: units}) do
+    units
+    |> Map.values()
+    |> IO.inspect(label: "remaining units")
+    |> Enum.map(& &1.hp)
+    |> Enum.sum()
+  end
 
   def space_type(%State{walls: walls, units: units}, space) do
     cond do
       MapSet.member?(walls, space) ->
         :wall
 
-      unit = Enum.find(units, fn %Unit{position: pos} -> pos == space end) ->
+      unit = Enum.find(Map.values(units), fn %Unit{position: pos} -> pos == space end) ->
         {:unit, unit}
 
       true ->
@@ -25,7 +40,7 @@ defmodule State do
     space_type(state, space) == :empty
   end
 
-  def draw(state = %State{walls: walls, units: units, bounds: {max_x, max_y}}, label \\ "") do
+  def draw(state = %State{bounds: {max_x, max_y}}, label \\ "") do
     str =
       for y <- 0..max_y do
         for x <- 0..max_x do
@@ -39,8 +54,16 @@ defmodule State do
       end
       |> Enum.join("\n")
 
-    IO.puts(label)
+    IO.puts(label <> "\n")
     IO.puts(str <> "\n")
+
+    # state.units
+    # |> Enum.sort_by(fn %Unit{position: {x, y}} -> {y, x} end)
+    # |> Enum.each(fn unit ->
+    #   IO.puts("#{inspect(unit.position)}: #{unit.hp}")
+    # end)
+
+    # IO.puts("\n")
 
     state
   end
@@ -48,157 +71,191 @@ end
 
 defmodule Pathfinder do
   def bfs(root, get_neighbors, target) do
-    IO.inspect(target, label: "trying to find")
+    queue = :queue.in({root, 0}, :queue.new())
 
-    queue = :queue.in({root, nil, 0}, :queue.new())
-
-    bfs(queue, get_neighbors, target, [], MapSet.new(), :infinity)
+    bfs(queue, get_neighbors, target, MapSet.new())
   end
 
-  defp bfs(queue, get_neighbors, target, paths, visited, actual_depth) do
-    # require IEx
-    # IEx.pry()
-
+  defp bfs(queue, get_neighbors, target, visited) do
     case :queue.out(queue) do
-      {{:value, tuple = {item, parent, depth}}, queue} ->
-        search(
-          tuple,
-          queue,
-          get_neighbors,
-          target,
-          paths,
-          MapSet.put(visited, item),
-          actual_depth
-        )
+      {{:value, tuple}, queue} ->
+        search(tuple, queue, get_neighbors, target, visited)
 
       {:empty, _queue} ->
-        {target, actual_depth, paths}
+        :no_path
     end
   end
 
-  defp search({_, _, depth}, _, _, target, paths, _, actual_depth)
-       when depth > actual_depth do
-    {target, actual_depth, paths}
+  defp search({target, depth}, _, _, target, _) do
+    {target, depth}
   end
 
-  defp search(
-         tuple = {target, parent, depth},
-         queue,
-         get_neighbors,
-         target,
-         paths,
-         visited,
-         actual_depth
-       ) do
-    bfs(queue, get_neighbors, target, [path_back(tuple) | paths], visited, depth)
-  end
-
-  defp search(
-         tuple = {item, parent, depth},
-         queue,
-         get_neighbors,
-         target,
-         paths,
-         visited,
-         actual_depth
-       ) do
+  defp search({item, depth}, queue, get_neighbors, target, visited) do
     queue =
       get_neighbors.(item)
-      |> Enum.map(fn neighbor -> {neighbor, tuple, depth + 1} end)
-      |> Enum.reduce(queue, fn tuple = {item, _, _}, queue ->
-        if MapSet.member?(visited, item) do
-          queue
-        else
-          :queue.in(tuple, queue)
-        end
+      |> Enum.reject(fn neighbor -> MapSet.member?(visited, neighbor) end)
+      |> Enum.map(fn neighbor -> {neighbor, depth + 1} end)
+      |> Enum.reject(fn tuple -> :queue.member(tuple, queue) end)
+      |> Enum.reduce(queue, fn tuple, queue ->
+        :queue.in(tuple, queue)
       end)
 
-    bfs(queue, get_neighbors, target, paths, visited, actual_depth)
-  end
-
-  defp path_back(tuple), do: path_back(tuple, [])
-
-  defp path_back({_, nil, _}, result), do: result
-
-  defp path_back({item, parent, _}, result) do
-    path_back(parent, [item | result])
+    bfs(queue, get_neighbors, target, MapSet.put(visited, item))
   end
 end
 
 defmodule Day15 do
   def part1 do
-    input()
-    |> State.draw()
-    |> after_rounds(4)
+    {state, last_round} =
+      input()
+      |> State.draw("initial state")
+      |> play()
+
+    State.total_hp(state) * last_round
   end
 
-  defp after_rounds(state, 0), do: state
+  defp play(state, bail_on_elf_death \\ false), do: play(state, 1, bail_on_elf_death)
 
-  defp after_rounds(state = %State{units: units}, round) do
-    units
-    |> Enum.sort_by(fn %Unit{position: {x, y}} -> {y, x} end)
-    |> Enum.reduce(state, &turn/2)
-    |> State.draw("#{round} rounds to go")
-    |> after_rounds(round - 1)
+  defp play(state = %State{units: units}, round, bail_on_elf_death) do
+    elves =
+      units
+      |> Map.values()
+      |> Enum.count(&(&1.species == :elf))
+
+    result =
+      units
+      |> Map.values()
+      |> Enum.sort_by(fn %Unit{position: {x, y}} -> {y, x} end)
+      |> Enum.map(& &1.id)
+      |> Enum.reduce_while(state, fn id, state ->
+        case Map.fetch(state.units, id) do
+          {:ok, unit} ->
+            case turn(unit, state) do
+              {:done, state} -> {:halt, {:done, state}}
+              state -> {:cont, state}
+            end
+
+          # this means the unit has died this turn
+          :error ->
+            {:cont, state}
+        end
+      end)
+
+    case result do
+      {:done, state} ->
+        State.draw(state, "THAT'S IT")
+        {state, round - 1}
+
+      state ->
+        elves_after =
+          state.units
+          |> Map.values()
+          |> Enum.count(&(&1.species == :elf))
+
+        if bail_on_elf_death and elves > elves_after do
+          State.draw(state, "ELF DIED")
+          {state, round - 1}
+        else
+          state
+          |> State.draw("After #{round} rounds")
+          |> play(round + 1, bail_on_elf_death)
+        end
+    end
   end
 
   defp turn(unit, state = %State{units: units}) do
-    if in_range?(unit, targets(unit, units)) do
-      combat(unit, state)
+    case targets(unit, units) do
+      [] -> {:done, state}
+      targets -> turn(unit, targets, state)
+    end
+  end
+
+  defp turn(unit, targets, state) do
+    if in_range?(unit, targets) do
+      combat(unit, targets, state)
     else
       move(unit, state)
     end
   end
 
-  defp in_range?(%Unit{position: {x, y}}, units) do
+  defp in_range_targets(%Unit{position: {x, y}} = unit, targets) do
     [
       {x - 1, y},
       {x + 1, y},
       {x, y - 1},
       {x, y + 1}
     ]
-    |> Enum.any?(fn space -> Enum.any?(units, fn unit -> unit.position == space end) end)
+    |> Enum.map(fn space -> Enum.find(targets, fn unit -> unit.position == space end) end)
+    |> Enum.reject(&(&1 == nil))
+  end
+
+  defp in_range?(unit, targets) do
+    Enum.any?(in_range_targets(unit, targets))
   end
 
   defp move(unit, state = %State{units: units}) do
+    all_targets = targets(unit, units)
+
     possible_targets =
-      unit
-      |> targets(units)
-      |> IO.inspect(label: "targets")
+      all_targets
       |> in_range(state)
-      |> Enum.map(fn target -> shortest_paths_to(unit.position, target, state) end)
-      |> IO.inspect(label: "paths")
-      |> Enum.reject(fn {_, distance, _} -> distance == :infinity end)
-      |> Enum.sort_by(fn {{x, y}, distance, _} -> {distance, {y, x}} end)
-      |> IO.inspect(label: "sorted paths")
+      |> Enum.map(fn target -> nearest_neighbors(unit.position, target, state) end)
+      |> Enum.reject(&(&1 == :no_path))
+      |> Enum.sort_by(fn {{x, y}, distance} -> {distance, {y, x}} end)
 
     case possible_targets do
-      [{_, _, paths} | _] -> move_along_path(unit, paths, state)
-      [] -> combat(unit, state)
+      [{target, _} | _] -> move_along_path(unit, target, state)
+      [] -> combat(unit, all_targets, state)
     end
   end
 
-  defp combat(unit, state) do
-    state
+  defp combat(unit, targets, state) do
+    in_range_targets(unit, targets)
+    |> Enum.sort_by(fn %Unit{hp: hp, position: {x, y}} -> {hp, y, x} end)
+    |> fight_targets(unit, state)
   end
 
-  defp move_along_path(unit, paths, %State{units: units} = state) do
-    [[first_step | _] | _] =
-      paths
-      |> IO.inspect(label: "paths to move")
-      |> Enum.sort_by(fn [{x, y} | _] -> {y, x} end)
+  defp fight_targets([], _, state), do: state
 
-    IO.inspect(first_step, label: "first step")
+  defp fight_targets([target | _], unit, %State{units: units} = state) do
+    hp = target.hp - unit.attack
 
-    %State{state | units: [%Unit{unit | position: first_step} | List.delete(units, unit)]}
+    units =
+      if hp > 0 do
+        Map.put(units, target.id, %Unit{target | hp: hp})
+      else
+        Map.drop(units, [target.id])
+      end
+
+    %State{state | units: units}
   end
 
-  defp shortest_paths_to(from, target, state) do
+  defp move_along_path(unit, target, %State{units: units} = state) do
+    [{first_step, _} | _] =
+      unit.position
+      |> empty_neighbors(state)
+      |> Enum.map(fn neighbor ->
+        {neighbor,
+         Pathfinder.bfs(neighbor, fn space -> empty_neighbors(space, state) end, target)}
+      end)
+      |> Enum.reject(fn {_, path} -> path == :no_path end)
+      |> Enum.map(fn {neighbor, {_, distance}} -> {neighbor, distance} end)
+      |> Enum.sort_by(fn {{x, y}, distance} -> {distance, y, x} end)
+
+    unit = %Unit{unit | position: first_step}
+    units = Map.put(units, unit.id, unit)
+
+    combat(unit, targets(unit, units), %State{state | units: units})
+  end
+
+  defp nearest_neighbors(from, target, state) do
     Pathfinder.bfs(from, fn space -> empty_neighbors(space, state) end, target)
   end
 
   defp targets(%Unit{species: species}, all_units) do
-    Enum.reject(all_units, &(&1.species == species))
+    all_units
+    |> Map.values()
+    |> Enum.reject(&(&1.species == species))
   end
 
   defp in_range(targets, state) do
@@ -206,7 +263,6 @@ defmodule Day15 do
     |> Enum.map(fn %Unit{position: position} -> position end)
     |> Enum.flat_map(&empty_neighbors(&1, state))
     |> Enum.dedup()
-    |> IO.inspect(label: "in-range spaces")
   end
 
   defp empty_neighbors({x, y}, state) do
@@ -220,30 +276,61 @@ defmodule Day15 do
   end
 
   def part2 do
+    until_elves_win()
   end
 
-  defp parse_char("#", x, y, state = %State{walls: walls}) do
+  def until_elves_win, do: until_elves_win(30)
+
+  def until_elves_win(elf_attack) do
+    state = input(elf_attack)
+
+    elves =
+      state.units
+      |> Map.values()
+      |> Enum.count(&(&1.species == :elf))
+
+    {state, last_round} =
+      state
+      |> State.draw("initial state")
+      |> play(true)
+
+    elves_after =
+      state.units
+      |> Map.values()
+      |> Enum.count(&(&1.species == :elf))
+
+    if elves == elves_after do
+      State.total_hp(state) * last_round
+    else
+      until_elves_win(elf_attack + 1)
+    end
+  end
+
+  defp parse_char("#", x, y, state = %State{walls: walls}, _) do
     %State{state | walls: MapSet.put(walls, {x, y})}
   end
 
-  defp parse_char(char, x, y, state = %State{units: units}) when char in ["E", "G"] do
-    unit = %Unit{position: {x, y}, species: unit_species(char)}
-    %State{state | units: [unit | units]}
+  defp parse_char(char, x, y, state = %State{units: units, next_id: id}, elf_attack)
+       when char in ["E", "G"] do
+    unit = parse_unit(char, id, {x, y}, elf_attack)
+    %State{state | units: Map.put(units, id, unit), next_id: id + 1}
   end
 
-  defp parse_char(_, _, _, state), do: state
+  defp parse_char(_, _, _, state, _), do: state
 
-  defp unit_species("E"), do: :elf
-  defp unit_species("G"), do: :goblin
+  defp parse_unit("E", id, position, elf_attack),
+    do: %Unit{id: id, position: position, species: :elf, attack: elf_attack}
 
-  defp parse_line(line, y, state) do
+  defp parse_unit("G", id, position, _), do: %Unit{id: id, position: position, species: :goblin}
+
+  defp parse_line(line, y, state, elf_attack) do
     line
     |> String.split("", trim: true)
     |> Enum.with_index()
-    |> Enum.reduce(state, fn {char, x}, state -> parse_char(char, x, y, state) end)
+    |> Enum.reduce(state, fn {char, x}, state -> parse_char(char, x, y, state, elf_attack) end)
   end
 
-  defp input do
+  defp input(elf_attack \\ 3) do
     lines =
       input_path()
       |> File.read!()
@@ -252,7 +339,7 @@ defmodule Day15 do
     state =
       lines
       |> Enum.with_index()
-      |> Enum.reduce(%State{}, fn {line, y}, state -> parse_line(line, y, state) end)
+      |> Enum.reduce(%State{}, fn {line, y}, state -> parse_line(line, y, state, elf_attack) end)
 
     bounds = {String.length(hd(lines)) - 1, Enum.count(lines) - 1}
 
@@ -266,5 +353,5 @@ defmodule Day15 do
   end
 end
 
-IO.puts("Part 1: #{Day15.part1()}")
+# IO.puts("Part 1: #{Day15.part1()}")
 IO.puts("Part 2: #{Day15.part2()}")
